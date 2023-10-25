fee47375e8b6af5ae03bfe58c7a1308590ad05b3
jgrewall <60413222+jgrewall@users.noreply.github.com>
2023-10-25 14:38:14 -0700

    Add files via upload

--- isAnagramUsingHashMap.java
+++ isAnagramUsingHashMap.java
@@ -1,18 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-public class Solution {
+public class AnagramChecker {
 
-    static boolean isAnagram(String a, String b) {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstString = scanner.next();
+        String secondString = scanner.next();
+        scanner.close();
+        
+        boolean result = checkAnagram(firstString, secondString);
+        System.out.println(result ? "Anagrams" : "Not Anagrams");
     }
 
-  public static void main(String[] args) {
-    
-        Scanner scan = new Scanner(System.in);
-        String a = scan.next();
-        String b = scan.next();
-        scan.close();
-        boolean ret = isAnagram(a, b);
-        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    private static boolean checkAnagram(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+
+        for (char ch : a.toLowerCase().toCharArray()) {
+            frequencyMap.put(ch, frequencyMap.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : b.toLowerCase().toCharArray()) {
+            if (!frequencyMap.containsKey(ch) || frequencyMap.get(ch) == 0) {
+                return false;
+            }
+            frequencyMap.put(ch, frequencyMap.get(ch) - 1);
+        }
+
+        return true;
     }
 }
