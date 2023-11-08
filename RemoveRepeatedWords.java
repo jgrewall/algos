b699e605214941b94ea074970ae3b65bea44b140
jgrewall <60413222+jgrewall@users.noreply.github.com>
2023-11-08 09:27:02 -0800

    Add files via upload

--- RemoveRepeatedWords.java
+++ RemoveRepeatedWords.java
@@ -1,20 +1,24 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Solution {
+public class DuplicateWords {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        String result = s.replaceAll("[,\\n]", "");
-        String[] ss = result.split("\\s|'|\\b\\?|!|,\\b|,$");
 
-        System.out.println(ss.length);
-
-        for (String word : ss) {
-            System.out.println(word);
+        String regex = "\\b(\\w+)(\\s+\\1\\b)+";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+            Matcher m = p.matcher(input);
+            while (m.find()) {
+                input = input.replaceAll("\\b" + m.group(1) + "\\b", m.group(1));
+            }
+            System.out.println(input);
         }
 
-        scan.close();
+        in.close();
     }
 }
